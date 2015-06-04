4a7a4e688fe110245730245b2529cc0a97e6a77a
Reatha <rea.tith@gmail.com>
2015-06-04 17:36:53 +0200

    Add class Cell (setters+getters)

--- src/lightbot/system/world/Cell.java
+++ src/lightbot/system/world/Cell.java
@@ -1,12 +1,47 @@
-/**
- * 
- */
 package lightbot.system.world;
 
-/**
- * @author Nasheis
- *
- */
-public class Cell {
+import lightbot.system.Colour;
 
+
+public class Cell {
+	
+	private int height;
+	private Colour colour;
+	
+	/**
+	 * Default constructor for a Cell
+	 */
+	public Cell(){
+		this.height = 1;
+		this.colour = Colour.WHITE;
+	}
+	
+	/**
+	 * Constructor for a Cell with level and colour
+	 * @param level
+	 * @param col
+	 */
+	public Cell(int level, Colour colour){
+		this.height = level;
+		this.colour = colour;
+	}
+	
+	public void setHeight(int level){
+		this.height = level;
+	}
+	
+	public void setColour(Colour colour){
+		this.colour = colour;
+	}
+	
+	public int getHeight(){
+		return this.height;
+	}
+	
+	public Colour getColour(){
+		return this.colour;
+	}
+	
 }
+
+
