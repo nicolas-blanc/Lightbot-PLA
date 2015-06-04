97b87ae7b660cbcaa06d546b04a8124ce42a41d7
Reatha <rea.tith@gmail.com>
2015-06-04 17:35:52 +0200

    Add class Robot (getters+setters)

--- src/lightbot/system/Robot.java
+++ src/lightbot/system/Robot.java
@@ -1,12 +1,111 @@
-/**
- * 
- */
 package lightbot.system;
 
-/**
- * @author Nasheis
- *
- */
+
 public class Robot {
+	
+	/* TODO : might change posX and posY to cellNb afterwards */
+	private int posX;
+	private int posY;
+	private Colour colour;
+	private Direction direction;
+	
+	/**
+	 * Default constructor for a Robot
+	 */
+	public Robot(){
+		this.posX = 0;
+		this.posY = 0;
+		colour = Colour.WHITE;
+		direction = Direction.UP;
+	}
+	
+	/**
+	 * Constructor for a Robot with positions x, y and colour
+	 * @param x : column in the grid
+	 * @param y : line in the grid
+	 * @param colour : initial colour
+	 */
+	public Robot(int x, int y, Colour colour, Direction direction){
+		this.posX = x;
+		this.posY = y;
+		this.colour = colour;
+		this.direction = direction;
+	}
+	
+	/**
+	 * Sets x and y
+	 * @param x
+	 * @param y
+	 */
+	public void setPosition(int x, int y){
+		this.posX = x;
+		this.posY = y;
+	}
+	
+	/**
+	 * Sets x
+	 * @param x
+	 */
+	public void setPositionX(int x){
+		this.posX = x;
+	}
+	
+	/**
+	 * Sets y
+	 * @param y
+	 */
+	public void setPositionY(int y){
+		this.posY = y;
+	}
+	
+	/**
+	 * Sets direction
+	 * @param direction
+	 */
+	public void setDirection(Direction direction){
+		this.direction = direction;
+	}
+	
+	/**
+	 * Colour setter
+	 * @param col : the colour to set
+	 */
+	public void setColour(Colour colour){
+		this.colour = colour;
+	}
+	
+	/**
+	 * Gets a robot's X position
+	 * @return posX
+	 */
+	public int getPositionX(){
+		return this.posX;
+	}
+	
+	/**
+	 * Gets a robot's Y position
+	 * @return posY
+	 */
+	public int getPositionY(){
+		return this.posY;
+	}
+	
+	/**
+	 * Gets the robot's colour
+	 * @return the colour
+	 */
+	public Colour getColour(){
+		return this.colour;
+	}
+	
+	/**
+	 * Gets direction
+	 * @param direction
+	 */
+	public Direction getDirection(){
+		return this.direction;
+	}
+	
+	
 
 }
