d9bcfbb9ce7f86d39bf22dd589da3b7a4d0b777e
Reatha <rea.tith@gmail.com>
2015-06-05 15:14:01 +0200

    Added functions for Grid

--- src/lightbot/system/world/Grid.java
+++ src/lightbot/system/world/Grid.java
@@ -1,15 +1,85 @@
-/**
- * 
- */
 package lightbot.system.world;
 
-/**
- * @author Nasheis
- *
- */
+import lightbot.system.Colour;
+import lightbot.system.Direction;
+
 public class Grid {
-	
+
 	// définir la "boussole" de la grille !!!!
+	// matrice de cellules
+
+	private int size;
+	private Cell[][] grid;
+
+	public Grid(int size) {
+		grid = new Cell[size][size];
+		for(int i=0; i<size; i++){
+			for(int j=0; j<size; j++){
+				grid[i][j] = new Cell(-1, Colour.WHITE, i, j);
+			}
+		}
+	}
 	
-	private Cell level[][];
+	public Grid(Grid gridToCopy, int size){
+		this(size);
+		for(int i=0; i<size; i++){
+			for(int j=0; j<size; j++){
+				Cell cellToCopy = gridToCopy.getCell(i,j);
+				this.setCell(i,j,cellToCopy.getHeight());
+			}
+		}
+	}
+
+	public Cell getCell(int x, int y) {
+		return this.grid[x][y];
+	}
+	
+	public void setCell(int x, int y, int level){
+		this.grid[x][y].setHeight(level);
+	}
+
+	/**
+	 * 
+	 * @param currentX
+	 * @param currentY
+	 * @param direction
+	 * @return
+	 * @throws OutOfGridException
+	 */
+	public Cell getNextCell(int currentX, int currentY, Direction direction) throws OutOfGridException {
+
+		switch (direction) {
+		case NORTH:
+			if (currentX == 0) {
+				throw new OutOfGridException();
+			}
+			return this.grid[currentX-1][currentY];
+		case SOUTH:
+			if (currentX == size-1){
+				throw new OutOfGridException();
+			}
+			return this.grid[currentX+1][currentY];
+		case WEST:
+			if (currentY == 0){
+				throw new OutOfGridException();
+			}
+			return this.grid[currentX][currentY-1];
+		case EAST:
+			if (currentY == size-1){
+				throw new OutOfGridException();
+			}
+			return this.grid[currentX][currentY+1];
+		default:
+			throw new OutOfGridException();
+		}
+	}
+	
+	public void printGrid(){
+		for(int i=0; i<grid.length; i++){
+			for(int j=0; j<grid[i].length; j++){
+				System.out.print(grid[i][j].getHeight() + "\t");
+			}
+			System.out.println();
+		}
+	}
 }
