6d27d5c92ab20784dd214f8bf3ab7220403b9dd8
AntoineBL <antoineblanc269@laposte.com>
2015-06-05 21:21:01 +0200

    Added Turn  functions

--- src/lightbot/system/action/Turn.java
+++ src/lightbot/system/action/Turn.java
@@ -1,12 +1,49 @@
-/**
- * 
- */
 package lightbot.system.action;
 
-/**
- * @author Nasheis
- *
- */
-public class Turn implements _Action {
+import lightbot.system.Direction;
+import lightbot.system.Robot;
 
+public class Turn implements _Action {
+	
+	/**
+	 * Calculates the new direction of the robot if it turn
+	 * @param robot
+	 * @param turn : direction he turns
+	 */
+	public static void execute(Robot robot, TurnDirection turn){
+		if(robot.getDirection() == Direction.NORTH && turn == TurnDirection.LEFT){
+			robot.setDirection(Direction.WEST);
+			return;
+		}		
+		if(robot.getDirection() == Direction.NORTH && turn == TurnDirection.RIGHT){
+			robot.setDirection(Direction.EAST);
+			return;
+		}
+		if(robot.getDirection() == Direction.SOUTH && turn == TurnDirection.LEFT){
+			robot.setDirection(Direction.EAST);
+			return;
+		}
+		if(robot.getDirection() == Direction.SOUTH && turn == TurnDirection.RIGHT){
+			robot.setDirection(Direction.WEST);
+			return;
+		}
+		if(robot.getDirection() == Direction.EAST && turn == TurnDirection.LEFT){
+			robot.setDirection(Direction.NORTH);
+			return;
+		}
+		if(robot.getDirection() == Direction.EAST && turn == TurnDirection.RIGHT){
+			robot.setDirection(Direction.SOUTH);
+			return;
+		}
+		if(robot.getDirection() == Direction.WEST && turn == TurnDirection.LEFT){
+			robot.setDirection(Direction.SOUTH);
+			return;
+		}
+		if(robot.getDirection() == Direction.WEST && turn == TurnDirection.RIGHT){
+			robot.setDirection(Direction.NORTH);
+			return;
+		}
+		
+		
+	}
 }
