ce94ad72b96102b91c4ef16b09ac0e907068a125
mhallal <hallal.marwan@gmail.com>
2015-06-08 18:09:09 +0200

    added test for the display of list of instructions

--- src/lightbot/tests/TestMarwan.java
+++ src/lightbot/tests/TestMarwan.java
@@ -1,8 +1,14 @@
 package lightbot.tests;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import lightbot.graphics.Textures;
 
+import org.jsfml.graphics.Color;
 import org.jsfml.graphics.RenderWindow;
+import org.jsfml.graphics.Sprite;
+import org.jsfml.graphics.Texture;
 import org.jsfml.window.VideoMode;
 import org.jsfml.window.event.Event;
 
@@ -10,8 +16,20 @@ public class TestMarwan {
 
 	public static RenderWindow window;
 
+	public static void renderInstructionsIcons(List<Texture> textures,
+			int posFirstTex, RenderWindow window) {
+		for (int i = 0; i < textures.size(); i++) {
+			Texture tex = textures.get(i);
+			Sprite s = new Sprite(tex);
+			float x = posFirstTex + i * tex.getSize().x + 6;
+			float y = 700;
+			s.setPosition(x, y);
+			window.draw(s);
+		}
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		boolean firstRendering = true;
 
 		Textures.initTextures();
 
@@ -19,13 +37,26 @@ public class TestMarwan {
 		window.create(new VideoMode(1280, 800), "Hello JSFML!");
 
 		window.setFramerateLimit(30);
-		
+
+		List<Texture> tex = new ArrayList<Texture>();
+		tex.add(Textures.forwardTexture);
+		tex.add(Textures.turnRightTexture);
+		tex.add(Textures.turnLeftTexture);
+		tex.add(Textures.lightTexture);
+		tex.add(Textures.jumpTexture);
+		tex.add(Textures.procedure1Texture);
+		tex.add(Textures.procedure2Texture);
 
 		while (window.isOpen()) {
-			window.clear();
+			if (firstRendering) {
+				firstRendering = false;
+				window.clear(new Color(216, 216, 216));
+			}
 			
+			renderInstructionsIcons(tex, 50, window);
+
 			window.display();
-			
+
 			for (Event event : window.pollEvents()) {
 				switch (event.type) {
 				case CLOSED:
