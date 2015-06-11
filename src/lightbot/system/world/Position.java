c62f938223853fe5743d76ec75eabf48654c3736
mhallal <hallal.marwan@gmail.com>
2015-06-11 16:48:42 +0200

    changed cell

---
+++ src/lightbot/system/world/Position.java
@@ -0,0 +1,21 @@
+package lightbot.system.world;
+
+public class Position {
+	
+	private int x;
+	private int y;
+	
+	public Position(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() {
+		return this.x;
+	}
+	
+	public int getY() {
+		return this.y;
+	}
+
+}
---
+++ src/lightbot/system/world/cell/Cell.java
@@ -0,0 +1,45 @@
+package lightbot.system.world.cell;
+
+import lightbot.system.world.Position;
+
+/**
+ * 
+ * Represents a basic cell defined only its position.
+ *
+ */
+public abstract class Cell {
+
+	
+	private Position position;
+
+	public Cell(int x, int y) {
+		this.position = new Position(x, y);
+	}
+
+	public Position getPosition() {
+		return this.position;
+	}
+
+	public int getX() {
+		return this.position.getX();
+	}
+
+	public int getY() {
+		return this.position.getY();
+	}
+
+	/**
+	 * @return true if cell is an EmptyCell.
+	 */
+	public boolean isEmptyCell() {
+		return this instanceof EmptyCell;
+	}
+	
+	public abstract void setLight(boolean lightON);
+	
+	public abstract boolean isLightON();
+	
+	public abstract int getHeight();
+	
+	
+}
