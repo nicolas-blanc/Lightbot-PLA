9e4be09224b3fe3c2af183182336d3354dbb9ad1
mhallal <hallal.marwan@gmail.com>
2015-06-16 12:06:38 +0200

    change clas name

--- src/lightbot/LightCore.java
+++ src/lightbot/LightCore.java
@@ -5,7 +5,7 @@ import java.util.List;
 import lightbot.graphics.DisplayMode;
 import lightbot.graphics.Editor;
 import lightbot.graphics.Game;
-import lightbot.graphics.ProcedureDisplay;
+import lightbot.graphics.ActionListDisplay;
 import lightbot.graphics.Textures;
 import lightbot.system.ParserJSON;
 import lightbot.system._Executable;
@@ -22,84 +22,87 @@ import org.jsfml.window.event.Event;
 import org.jsfml.window.event.TextEvent;
 
 public class LightCore {
-	
+
 	public static RenderWindow window;
 	public static DisplayMode display;
-	
+
 	public static VideoMode screenInformations;
 	public static float scaleRatio;
-	
+
 	public static int originX;
 	public static int originY;
 
 	public static void main(String[] args) {
 		
+		boolean firstLaunch = true;
+
 		Textures.initTextures();
-		
-		//Create the window
+
+		// Create the window
 		window = new RenderWindow();
-		
+
 		screenInformations = VideoMode.getDesktopMode();
-		scaleRatio = ((float)screenInformations.width)/1920;
+		scaleRatio = ((float) screenInformations.width) / 1920;
 		System.out.println(scaleRatio);
-		
+
 		// base 1280 * 960 for 1920*1080
 		window.create(new VideoMode(1000, 600), "LightCore");
-		
-		//Limit the framerate
+
+		// Limit the framerate
 		window.setFramerateLimit(60);
-		
-		//WorldGenerator newWorld = new WorldGenerator();
-		
-		//newWorld.getGrid().printGrid();
-		//Grid grid = newWorld.getGrid();
+
+		// WorldGenerator newWorld = new WorldGenerator();
+
+		// newWorld.getGrid().printGrid();
+		// Grid grid = newWorld.getGrid();
 		Level level = ParserJSON.deserialize("example.json");
 		Grid grid = level.getGrid();
 		List<_Executable> actions = level.getListOfActions();
 
-		//display = new Editor(5);
+		// display = new Editor(5);
 		display = new Game(grid);
-		//display.printGrid();
-		
-		//RectangleShape rect = new RectangleShape(new Vector2f(750, 460));
+		// display.printGrid();
+
+		// RectangleShape rect = new RectangleShape(new Vector2f(750, 460));
 		RectangleShape rect = new RectangleShape(new Vector2f(710, 475));
 		rect.setPosition(15, 15);
 		rect.setOutlineThickness(1);
 		rect.setOutlineColor(Color.BLACK);
-		
-		//Main loop
-		while (window.isOpen()) {
-		    //Draw everything
-		    window.clear(Color.WHITE);
-		    window.draw(rect);
-		    //display.display();
-		    ProcedureDisplay.displayActionList(actions, 125, window);
-		   
-		   
 
-		  //Handle events
-		    for(Event event : window.pollEvents()) {
-		    	display.eventManager(event);
-		    	 switch(event.type) {
-			         case CLOSED:
-			             System.out.println("The user pressed the close button!");
-			             window.close();
-			             break;
-			        	    
-			         /*case TEXT_ENTERED:
-			        	    TextEvent textEvent = event.asTextEvent();
-			        	    System.out.println("The user typed the following character: " + textEvent.character);
-			        	    break;
-	
-			         case MOUSE_WHEEL_MOVED:
-			             System.out.println("The user moved the mouse wheel!");
-			             break;*/
-					default:
-						break;
-			     }
-		    }
-		    
-		    window.display();
+		// Main loop
+		while (window.isOpen()) {
+			if(firstLaunch) {
+				firstLaunch = false;
+			}
+			// Draw everything
+			window.clear(Color.WHITE);
+			window.draw(rect);
+			display.display();
+			ActionListDisplay.displayActionList(actions, 10, window);
+
+			// Handle events
+			for (Event event : window.pollEvents()) {
+				display.eventManager(event);
+				switch (event.type) {
+				case CLOSED:
+					System.out.println("The user pressed the close button!");
+					window.close();
+					break;
+				/*
+				 * case TEXT_ENTERED: TextEvent textEvent = event.asTextEvent();
+				 * System.out.println("The user typed the following character: "
+				 * + textEvent.character); break;
+				 * 
+				 * case MOUSE_WHEEL_MOVED:
+				 * System.out.println("The user moved the mouse wheel!"); break;
+				 */
+				
+				default:
+					break;
+				}
+			}
+
+			window.display();
 		}
 	}
 }
