0c0754136f04a1775acdc983aeb7c41946166ac2
mhallal <hallal.marwan@gmail.com>
2015-06-18 11:35:27 +0200

    add animation to actions

--- src/lightbot/system/action/Turn.java
+++ src/lightbot/system/action/Turn.java
@@ -1,5 +1,7 @@
 package lightbot.system.action;
 
+import lightbot.LightCore;
+import lightbot.graphics.Game;
 import lightbot.system.CardinalDirection;
 import lightbot.system.Colour;
 import lightbot.system.Robot;
@@ -10,60 +12,78 @@ public class Turn extends _Action {
 
 	RelativeDirection direction;
 
-	public Turn(){
+	public Turn() {
 		super(Colour.WHITE);
 	}
-	
-	public RelativeDirection getDirection(){return this.direction;}
-	
+
+	public RelativeDirection getDirection() {
+		return this.direction;
+	}
+
 	public Turn(RelativeDirection direction, Colour colour) {
 		super(colour);
 		this.direction = direction;
 	}
 
 	public void execute(Grid grid, Robot robot) {
-		if(robot.getColour() != this.colour && this.colour != Colour.WHITE){
+		if (robot.getColour() != this.colour && this.colour != Colour.WHITE) {
 			return;
 		}
-		
-		if (robot.getDirection() == CardinalDirection.NORTH
-				&& direction == RelativeDirection.LEFT) {
+
+		if (robot.getDirection() == CardinalDirection.NORTH && direction == RelativeDirection.LEFT) {
 			robot.setDirection(CardinalDirection.WEST);
+			((Game) LightCore.display).display.robotDisplay.updateRobot(Robot.wheatley, 255);
+			((Game) LightCore.display).display.anim
+					.updateRobot(((Game) LightCore.display).display.robotDisplay.robotSprite);
 			return;
 		}
-		if (robot.getDirection() == CardinalDirection.NORTH
-				&& direction == RelativeDirection.RIGHT) {
+		if (robot.getDirection() == CardinalDirection.NORTH && direction == RelativeDirection.RIGHT) {
 			robot.setDirection(CardinalDirection.EAST);
+			((Game) LightCore.display).display.robotDisplay.updateRobot(Robot.wheatley, 255);
+			((Game) LightCore.display).display.anim
+					.updateRobot(((Game) LightCore.display).display.robotDisplay.robotSprite);
 			return;
 		}
-		if (robot.getDirection() == CardinalDirection.SOUTH
-				&& direction == RelativeDirection.LEFT) {
+		if (robot.getDirection() == CardinalDirection.SOUTH && direction == RelativeDirection.LEFT) {
 			robot.setDirection(CardinalDirection.EAST);
+			((Game) LightCore.display).display.robotDisplay.updateRobot(Robot.wheatley, 255);
+			((Game) LightCore.display).display.anim
+					.updateRobot(((Game) LightCore.display).display.robotDisplay.robotSprite);
 			return;
 		}
-		if (robot.getDirection() == CardinalDirection.SOUTH
-				&& direction == RelativeDirection.RIGHT) {
+		if (robot.getDirection() == CardinalDirection.SOUTH && direction == RelativeDirection.RIGHT) {
 			robot.setDirection(CardinalDirection.WEST);
+			((Game) LightCore.display).display.robotDisplay.updateRobot(Robot.wheatley, 255);
+			((Game) LightCore.display).display.anim
+					.updateRobot(((Game) LightCore.display).display.robotDisplay.robotSprite);
 			return;
 		}
-		if (robot.getDirection() == CardinalDirection.EAST
-				&& direction == RelativeDirection.LEFT) {
+		if (robot.getDirection() == CardinalDirection.EAST && direction == RelativeDirection.LEFT) {
 			robot.setDirection(CardinalDirection.NORTH);
+			((Game) LightCore.display).display.robotDisplay.updateRobot(Robot.wheatley, 255);
+			((Game) LightCore.display).display.anim
+					.updateRobot(((Game) LightCore.display).display.robotDisplay.robotSprite);
 			return;
 		}
-		if (robot.getDirection() == CardinalDirection.EAST
-				&& direction == RelativeDirection.RIGHT) {
+		if (robot.getDirection() == CardinalDirection.EAST && direction == RelativeDirection.RIGHT) {
 			robot.setDirection(CardinalDirection.SOUTH);
+			((Game) LightCore.display).display.robotDisplay.updateRobot(Robot.wheatley, 255);
+			((Game) LightCore.display).display.anim
+					.updateRobot(((Game) LightCore.display).display.robotDisplay.robotSprite);
 			return;
 		}
-		if (robot.getDirection() == CardinalDirection.WEST
-				&& direction == RelativeDirection.LEFT) {
+		if (robot.getDirection() == CardinalDirection.WEST && direction == RelativeDirection.LEFT) {
 			robot.setDirection(CardinalDirection.SOUTH);
+			((Game) LightCore.display).display.robotDisplay.updateRobot(Robot.wheatley, 255);
+			((Game) LightCore.display).display.anim
+					.updateRobot(((Game) LightCore.display).display.robotDisplay.robotSprite);
 			return;
 		}
-		if (robot.getDirection() == CardinalDirection.WEST
-				&& direction == RelativeDirection.RIGHT) {
+		if (robot.getDirection() == CardinalDirection.WEST && direction == RelativeDirection.RIGHT) {
 			robot.setDirection(CardinalDirection.NORTH);
+			((Game) LightCore.display).display.robotDisplay.updateRobot(Robot.wheatley, 255);
+			((Game) LightCore.display).display.anim
+					.updateRobot(((Game) LightCore.display).display.robotDisplay.robotSprite);
 			return;
 		}
 
